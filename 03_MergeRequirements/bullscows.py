ff26c4359cd4856507ec7ec5aff5148237372228
Wrong_Wizzli <dima.vartan@gmail.com>
2022-03-06 17:28:16 +0300

    Inform and gameplay added

--- 03_MergeRequirements/bullscows.py
+++ 03_MergeRequirements/bullscows.py
@@ -1,5 +1,6 @@
 import textdistance
-
+from random import randint
+import sys
 
 def bullscows(guess: str, secret: str) -> (int, int):
     bulls = len(guess) - textdistance.hamming(guess, secret)
@@ -15,3 +16,17 @@ def ask(prompt: str, valid: list[str] = None) -> str:
             in_word = input(prompt)
     return in_word
 
+
+def inform(format_string: str, bulls: int, cows: int) -> None:
+    print(str.format(bulls, cows))
+    
+    
+def gameplay(ask: callable, inform: callable, words: list[str]) -> int:
+    secret_word = words[randint(0, len(words))]
+    c, counter = 0, 0
+    while c != len(secret_word): 
+        in_word = ask("Введите слово:", words)
+        counter += 1
+        b, c = bullscows(in_word, secret_word)
+        inform("Быки: {}, Коровы: {}", b, c)
+    return counter
