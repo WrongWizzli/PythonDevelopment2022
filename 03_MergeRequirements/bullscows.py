a4d7460a6a441b9ef48de82ece281c4cee9af6d6
Wrong_Wizzli <dima.vartan@gmail.com>
2022-03-06 17:27:33 +0300

    Ask function added

--- 03_MergeRequirements/bullscows.py
+++ 03_MergeRequirements/bullscows.py
@@ -5,3 +5,13 @@ def bullscows(guess: str, secret: str) -> (int, int):
     bulls = len(guess) - textdistance.hamming(guess, secret)
     cows = len(guess) - textdistance.bag(guess, secret)
     return (bulls, cows)
+
+
+def ask(prompt: str, valid: list[str] = None) -> str:
+    if valid is not None:
+        print("Доступные слова:", valid)
+        in_word = input(prompt)
+        while in_word not in valid:
+            in_word = input(prompt)
+    return in_word
+
